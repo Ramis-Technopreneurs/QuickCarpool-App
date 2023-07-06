0d3300ec5245da645aa4a8c2bcebd155dd700b66
dkrmentor <dkrmentor@gmail.com>
2023-07-06 19:42:15 +0500

    db-update

--- Database/carpool_rides.sql
+++ Database/carpool_rides.sql
@@ -16,28 +16,40 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `roles`
+-- Table structure for table `rides`
 --
 
-DROP TABLE IF EXISTS `roles`;
+DROP TABLE IF EXISTS `rides`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `roles` (
+CREATE TABLE `rides` (
   `id` int NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
   `create_time` datetime DEFAULT NULL COMMENT 'Create Time',
-  `name` varchar(255) DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `drive_id` int DEFAULT NULL,
+  `from_lat` double DEFAULT NULL,
+  `from_lng` double DEFAULT NULL,
+  `to_lat` double DEFAULT NULL,
+  `to_lng` double DEFAULT NULL,
+  `from_address` varchar(255) DEFAULT NULL,
+  `to_address` varchar(255) DEFAULT NULL,
+  `car_type` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `initial_waiting` double DEFAULT NULL,
+  `distance` double DEFAULT NULL,
+  `time` double DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `roles`
+-- Dumping data for table `rides`
 --
 
-LOCK TABLES `roles` WRITE;
-/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
-INSERT INTO `roles` VALUES (1,NULL,'passenger'),(2,NULL,'passenger&rider');
-/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+LOCK TABLES `rides` WRITE;
+/*!40000 ALTER TABLE `rides` DISABLE KEYS */;
+INSERT INTO `rides` VALUES (1,NULL,45,NULL,47.2312,47.3221,29.23133,47.3221,'house no','house no','mini','pending',NULL,NULL,NULL),(2,'2023-05-11 16:50:27',45,NULL,47.2312,47.3221,29.23133,47.3221,'house no','house no','mini','pending',24.2,24.2,24.2),(3,'2023-05-29 17:16:10',45,NULL,47.2312,47.3221,29.23133,47.3221,'house no','house no','mini','pending',24.2,24.2,24.2),(4,'2023-05-30 20:57:05',80,NULL,47.3212,125,29.1234,123.4154,'karachi','hyd','car','pending',24.4,1,2.5),(5,'2023-05-30 20:57:46',80,NULL,47.3212,125,29.1234,123.4154,'karachi','hyd','car','pending',24.4,1,2.5);
+/*!40000 ALTER TABLE `rides` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -49,4 +61,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-03 21:54:56
+-- Dump completed on 2023-07-06 19:29:20
